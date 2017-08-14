0a73dc7abbeaaefdcf3d49e8f571e672ec295ef7
colejd <joncole04412@gmail.com>
2017-08-14 18:03:51 -0400

    Update Guify to 0.4.0

--- src/main.js
+++ src/main.js
@@ -2,17 +2,28 @@ import { Detector } from "./utils/webgl-detect.js";
 import { Dust } from "./dust.js";
 import { gui } from "./gui.js";
 
-let container = document.getElementById("dust-container");
-
-if ( !Detector.HasWebGL() ) {
-    //exit("WebGL is not supported on this browser.");
-    console.log("WebGL is not supported on this browser.");
-    container.innerHTML = Detector.GetErrorHTML();
-    container.classList.add("no-webgl");
+let Init = () => {
+    let container = document.getElementById("dust-container");
+    if(!container) throw new Error("No #dust-container was found");
+    
+    if ( !Detector.HasWebGL() ) {
+        //exit("WebGL is not supported on this browser.");
+        console.log("WebGL is not supported on this browser.");
+        container.innerHTML = Detector.GetErrorHTML();
+        container.classList.add("no-webgl");
+    }
+    else {
+        let dust = new Dust(container, () => {
+            // Dust is now fully loaded
+            gui.Init(dust, container);
+        });
+    }
 }
-else {
-    let dust = new Dust(container, () => {
-        // Dust is now fully loaded
-        gui.Init(dust, container);
-    });
+
+if (document.readyState === 'complete') {
+    Init();
+} else {
+    window.onload = () => {
+        Init();
+    }
 }
